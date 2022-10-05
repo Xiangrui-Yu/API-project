593dacbdb57be5f3f4e0028b0520fa77d24c3ace
Ray Yu <RayYu@Lucky.com>
2022-10-04 20:17:15 -0400

    phase0

--- backend/app.js
+++ backend/app.js
@@ -5,41 +5,48 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-
 const routes = require('./routers');
 
-const {environment} = require('./config');
-const isProduction = environment === 'production'
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
 
 const app = express();
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(routes);
 
- if(!isProduction) {
-    app.use(cors());
+
+
+
+// Security Middleware
+if (!isProduction) {
+   // enable cors only in development
+   app.use(cors());
  }
-
+ 
+ // helmet helps set a variety of headers to better secure your app
  app.use(
-    helmet.crossOriginResourcePolicy({ 
-      policy: "cross-origin" 
-    })
-  );
-
- app.use(
-    csurf({
-        cookie:{
-            secure: isProduction,
-            sameSite: isProduction && 'Lax',
-            httpOnly:true
-        }
-    })
+   helmet.crossOriginResourcePolicy({ 
+     policy: "cross-origin" 
+   })
  );
+ 
+ // Set the _csrf token and create req.csrfToken method
+ app.use(
+   csurf({
+     cookie: {
+       secure: isProduction,
+       sameSite: isProduction && "Lax",
+       httpOnly: true
+     }
+   })
+ );
+ 
 
 
-
+ app.use(routes); 
 
  module.exports = app;
