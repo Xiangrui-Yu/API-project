e4e878e3b0edd045ad85724433ab0f378f8e152e
Ray Yu <RayYu@Lucky.com>
2022-10-11 19:22:41 -0400

    issue with auth

--- backend/routers/api/users.js
+++ backend/routers/api/users.js
@@ -9,53 +9,53 @@ const router = express.Router();
 
 
 router.post(
-    '/',
-    async (req, res) => {
-      const { email, password, username } = req.body;
-      const user = await User.signup({ email, username, password });
-  
-      await setTokenCookie(res, user);
-  
-      return res.json({
-        user
-      });
-    }
-  );
+  '/',
+  async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signup({ email, username, password });
 
-  const validateSignup = [
-    check('email')
-      .exists({ checkFalsy: true })
-      .isEmail()
-      .withMessage('Please provide a valid email.'),
-    check('username')
-      .exists({ checkFalsy: true })
-      .isLength({ min: 4 })
-      .withMessage('Please provide a username with at least 4 characters.'),
-    check('username')
-      .not()
-      .isEmail()
-      .withMessage('Username cannot be an email.'),
-    check('password')
-      .exists({ checkFalsy: true })
-      .isLength({ min: 6 })
-      .withMessage('Password must be 6 characters or more.'),
-    handleValidationErrors
-  ];
+    await setTokenCookie(res, user);
 
-  router.post(
-    '/',
-    validateSignup,
-    async (req, res) => {
-      const { email, password, username } = req.body;
-      const user = await User.signup({ email, username, password });
-  
-      await setTokenCookie(res, user);
-  
-      return res.json({
-        user,
-      });
-    }
-  );
+    return res.json({
+      user
+    });
+  }
+);
+
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors
+];
+
+router.post(
+  '/',
+  validateSignup,
+  async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signup({ email, username, password });
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user,
+    });
+  }
+);
 
 
 
