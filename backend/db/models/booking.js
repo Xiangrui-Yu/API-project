c7961eaa993449f006771abfc893436c086f8a5b
Ray Yu <RayYu@Lucky.com>
2022-10-12 18:53:38 -0400

    booking association

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -12,7 +12,13 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Booking.belongsTo(
-        
+        models.Spot,
+        {foreignKey: 'spotId'}
+      )
+
+      Booking.belongsTo(
+        models.User,
+        {foreignKey:'userId'}
       )
     }
   }
