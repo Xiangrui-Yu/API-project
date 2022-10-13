f1d5ee0076d722284ed155ef59a601e762da95ae
Ray Yu <RayYu@Lucky.com>
2022-10-13 19:15:09 -0400

    get all spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,22 +1,50 @@
 const express = require('express');
-const { setTokenCookie, restoreUser } = require('../../utils/auth');
-const {Spot,User,SpotImage,Review,Booking} = require('../../db/models');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+const { Spot, User, SpotImage, Review, Booking } = require('../../db/models');
 const router = express.Router();
 
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+const { urlencoded } = require('express');
 
 
 
-router.get('/',async(req,res,next) =>{
+router.get('/', async (req, res, next) => {
 
-    
     const allSpots = await Spot.findAll({
-    });    
+        include: [
+            {
+                model: Review
+            },
+            {
+                model: SpotImage
+            }
+        ]
+
+    });
+
+    const newSpots = [];
+
+    allSpots.forEach(ele => {
+        
+        newSpots.push(ele.toJSON())
+    })
 
 
-    res.json(allSpots)
+    newSpots.forEach(spot => {
+        spot.avgRating = spot.Reviews[0].stars;
+        spot.previewImage = spot.SpotImages[0].url;
+        delete spot.Reviews;
+        delete spot.SpotImages
+    })
+    
+
+    res.json({
+        Spot: newSpots
+
+
+    })
 })
 
 
