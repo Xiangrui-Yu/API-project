f88ff37cfd020623bf8283149a28c24869c504d5
Ray Yu <RayYu@Lucky.com>
2022-10-16 13:30:58 -0400

    review first CRUD

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
-const { Spot, User, SpotImage, Review, Booking } = require('../../db/models');
+const { Spot, User, SpotImage, Review, Booking, ReviewImage } = require('../../db/models');
 const router = express.Router();
 
 
@@ -8,9 +8,57 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { urlencoded } = require('express');
 
+// ### Get all Reviews of the Current User
+router.get('/current', requireAuth, async (req, res, next) => {
+    const currentUser = req.user.id
 
-router.get('/current',requireAuth,async(req,res,next) => {
-    res.json({})
+    const thisReview = await Review.findOne({
+        include: [{
+            model: User,
+            attributes: {
+                exclude: ['username', 'hashedPassword', 'email', 'createdAt', 'updatedAt']
+            }
+        },
+        {
+            model:Spot,
+            include:{
+                model:SpotImage
+            },
+            attributes:{
+                exclude: ['createdAt','updatedAt']
+            }
+           
+        },
+        {
+            model:ReviewImage,
+            attributes:{
+                exclude: ['reviewId','createdAt','updatedAt']
+            }
+        },
+        
+        ]
+    })
+
+    const thisReviewPojo = thisReview.toJSON();
+
+    const spotImageTable = thisReviewPojo.Spot.SpotImages;
+
+    if(spotImageTable){
+        if(spotImageTable[0].preview){
+            thisReviewPojo.Spot.previewImage = spotImageTable[0].url
+        }else{
+            thisReviewPojo.Spot.previewImage = 'no preview image'
+        }
+    }else{
+        thisReviewPojo.Spot.previewImage = 'no preview image'
+    }
+
+    delete thisReviewPojo.Spot.SpotImages
+
+
+    res.json({
+        Reviews:[thisReviewPojo]
+    })
 })
 
 
