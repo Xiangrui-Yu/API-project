6f4951cef9c3ea59966f8f66dc4992c5cd53951d
Ray Yu <RayYu@Lucky.com>
2022-10-17 00:13:59 -0400

    edit review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -116,6 +116,57 @@ router.post('/:reviewId/images',requireAuth,async(req,res,next) =>{
 
 })
 
+//### Edit a Review
+
+router.put('/:reviewId',requireAuth,async(req,res,next) =>{
+    const reviewId = req.params.reviewId;
+    const userId = req.user.id;
+
+    const {review,stars} = req.body;
+
+
+    const theReview = await Review.findByPk(reviewId);
+
+    if(!theReview){
+        res.status(404);
+        res.json( {
+            "message": "Review couldn't be found",
+            "statusCode": 404
+          })
+    }
+
+    if(userId === theReview.userId){
+        try {
+            theReview.set({
+                review:review,
+                stars:stars
+            })
+        
+            await theReview.save()
+        
+            res.json(theReview)
+            
+        } catch (error) {
+            res.status(400),
+            res.json(   {
+                "message": "Validation error",
+                "statusCode": 400,
+                "errors": {
+                  "review": "Review text is required",
+                  "stars": "Stars must be an integer from 1 to 5",
+                }
+              })
+        }
+       
+    } else{
+        res.status(403);
+        res.json( {
+            "message": "Forbidden",
+            "statusCode": 403
+          })
+    }  
+  
+})
 
 
 module.exports = router;
