0246680f6c2bd0be35edcc4587382b174232439b
Ray Yu <RayYu@Lucky.com>
2022-11-19 16:57:44 -0500

    loginform

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,6 +10,34 @@ router.use('/api', apiRouter);
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
 // });
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+
+router.use(express.static(path.resolve("../frontend/build")));
+
+router.get(/^(?!\/?api).*/, (req, res) => {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  return res.sendFile(
+    path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+  );
+});
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+}
+
+// old backend code below
 
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
